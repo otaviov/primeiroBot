6dac492590d653a13df3161f1bc8576ff6a4d79d
otaviov <amandavv32@gmail.com>
2021-10-07 22:09:29 -0300

    Create botoes.js

---
+++ exercicios/botoes.js
@@ -0,0 +1,10 @@
+/*  criando teclado inline
+    botões que geram ações
+*/
+
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const bot = new Telegraf(env.token)
+
