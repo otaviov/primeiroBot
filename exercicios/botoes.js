4e982f6c357c19cb1c0b8c9c8f27322379ee0a62
otaviov <amandavv32@gmail.com>
2021-10-08 21:16:57 -0300

    bug

--- exercicios/botoes.js
+++ exercicios/botoes.js
@@ -2,7 +2,7 @@
     botões que geram ações
 */
 
-const env = require('../.env')
+const env = require('../../.env')
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
