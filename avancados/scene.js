13bc5a84b5d1d9366009ddc3b25a7b1a3b8df788
otaviov <amandavv32@gmail.com>
2021-10-27 21:10:35 -0300

    Create scene.js

---
+++ avancados/scene.js
@@ -0,0 +1,6 @@
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Scene = require('telegraf/scenes/base')
+const bot = new Telegraf (env.token)
